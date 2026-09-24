46cb55034b5e952803a922989f99883167e2613f
Jesse Stay <jesse@staynalive.com>
2026-09-24 16:37:15 -0600

    Beacon v0.3.1: Beacon.Tests.ps1

--- Beacon.Tests.ps1
+++ Beacon.Tests.ps1
@@ -119,8 +119,8 @@ Describe "Beacon.ps1 encoding (the v0.2.0 install bug)" {
         $bad.Count | Should -Be 0
     }
 
-    It "reports version 0.3.0" {
-        $script:BeaconVersion | Should -Be "0.3.0"
+    It "reports version 0.3.1" {
+        $script:BeaconVersion | Should -Be "0.3.1"
     }
 }
 
